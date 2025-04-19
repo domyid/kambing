15820d2c794ac351b5bfa9832f79b4540065bf49
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-04-19 20:53:53 +0700

    update index & main

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ import {getHash} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 import {get,postWithToken} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.6/croot.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 
-
-// get("https://asia-southeast2-awangga.cloudfunctions.net/domyid/api/activityscore","login",getCookie("login"))
-// get("https://api.do.my.id/notif/ux/getlaporan/"+getHash(),runafterGet)
 get("https://asia-southeast2-awangga.cloudfunctions.net/domyid/data/proyek/bimbingan/"+getHash(),runafterGet)
 
 onClick("tombol",runOnRating);
+
+export async function main(){
+    fetchActivityScore();
+}
 
 function runafterGet(result){
     console.log(result);
@@ -16,6 +17,39 @@ function runafterGet(result){
     // Mengganti \n dengan <br> untuk menampilkan baris baru
     let solusiDenganBarisBaru = result.solusi.replace(/\n/g, "<br>");
     setInner("solusi", solusiDenganBarisBaru);
+}
+
+function fetchActivityScore() {
+    const url = "https://asia-southeast2-awangga.cloudfunctions.net/domyid/data/proyek/bimbingan/" + getHash();
+    get(url, handleActivityScoreResponse);
+}
+
+function handleActivityScoreResponse(result) {
+    if (result.status === 200) {
+        updateTableRow(1, result.data.stravakm, result.data.strava);
+        updateTableRow(2, result.data.iqresult, result.data.iq);
+        // updateTableRow(3, result.data.pomokitsesi, result.data.pomokit);
+        updateTableRow(6, result.data.trackerdata, result.data.tracker);
+        // updateTableRow(9, result.data.gtmetrixresult, result.data.gtmetrix);
+        updateTableRow(10, result.data.webhookpush, result.data.webhook);
+        updateTableRow(11, result.data.presensihari, result.data.presensi);
+    } else {
+        console.log(result.data.message);
+    }
+}
+
+function updateTableRow(rowIndex, quantity, points) {
+    const tableRows = document.querySelectorAll('table.table tbody tr');
+    const row = tableRows[rowIndex]; // Ambil baris berdasarkan indeks
+    if (row) {
+        const quantityCell = row.querySelector('td:nth-child(3)');
+        const pointsCell = row.querySelector('td:nth-child(4)');
+
+        if (quantityCell && pointsCell) {
+            quantityCell.textContent = quantity || 0;
+            pointsCell.textContent = points || 0;
+        }
+    }
 }
 
 function runOnRating(){
